e0d517784ec3210b0b1704c35694409f3ac657c5
pivonski <65859431+pivonski@users.noreply.github.com>
2023-03-06 21:53:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const converter = document.querySelector('#converter')
+const result = document.querySelector('.result')
+const convBtn = document.querySelector('.conv')
+const resetBtn = document.querySelector('.reset')
+const changeBtn = document.querySelector('.change')
+const one = document.querySelector('.one')
+const two = document.querySelector('.two')
+
+let fahrenheit
+let celsius
+
+const change = () => {
+	if (one.textContent === '°C') {
+		one.textContent = '°F'
+		two.textContent = '°C'
+		result.textContent = ''
+	} else {
+		one.textContent = '°C'
+		two.textContent = '°F'
+		result.textContent = ''
+	}
+}
+
+const fahrToCelc = () => {
+	fahrenheit = converter.value * 1.8 + 32
+	result.textContent = `${converter.value}°C to ${fahrenheit.toFixed(1)}°F`
+	converter.value = ''
+}
+const celcToFahr = () => {
+	celsius = (converter.value - 32) / 1.8
+	result.textContent = `${converter.value}°F to ${celsius.toFixed(1)}°C`
+	converter.value = ''
+}
+const convertion = () => {
+	if (converter.value !== '') {
+		if (one.textContent === '°C') {
+			fahrToCelc()
+		} else {
+			celcToFahr()
+		}
+	} else {
+		result.textContent = 'Podaj wartość'
+	}
+}
+const reset = () => {
+	result.textContent = ''
+	converter.textContent = ''
+}
+
+changeBtn.addEventListener('click', change)
+
+convBtn.addEventListener('click', convertion)
+
+resetBtn.addEventListener('click', reset)
